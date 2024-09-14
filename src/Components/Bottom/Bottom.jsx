83619ad653fc0d7ed9c3a9c43f9651d6cc8489f2
israr-faridi <vbmovielover465@gmail.com>
2024-09-15 03:27:46 +0500

    our_client_feedback_session

--- src/Components/Bottom/Bottom.jsx
+++ src/Components/Bottom/Bottom.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BottomHeading from '../../Common/BottomHeading/BottomHeading'
 import './Bottom.css'
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Cards } from '../Cards/Cards'
 import { SmallCard1, SmallCard2, SmallCard3 } from '../../Utiles/constant/cards'
 // import DoubleCard from '../Cards/DoubleCard'
@@ -43,7 +44,7 @@ const Bottom = () => {
       </div>
 
 
-      <BottomHeading title={"How To Order?"}  />
+      <BottomHeading title={"How To Order?"} />
 
       <div className="card_Wrapper3">
         {SmallCard3.map((item, index) => {
@@ -54,7 +55,27 @@ const Bottom = () => {
           )
         })}
       </div>
-      
+
+
+      <BottomHeading title={"Our Clients Feedback"} text={"The food at your doorstep. Why starve when you have us. You hunger partner. Straight out of the oven to your doorstep."} />
+
+
+      <div className="comments">
+        <img className='comments_Image' src="/assets/images/Vector.png" alt="image" />
+        <p className='commentsPara'><i>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi”</i></p>
+        <h4 className='author_comments'>Mitchell Marsh</h4>
+        <p className='author_position'>CEO, Bexon Agency</p>
+
+        <div className="arrows">
+          <div className="arrow">
+          <IoIosArrowBack />
+          </div>
+          <div className="arrow active">
+          <IoIosArrowForward />
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
